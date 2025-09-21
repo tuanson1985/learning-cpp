fbcffc2b00c06aa8029098e3dac0a8e41957a17c
tuansonhq <105621622+tuanson1985@users.noreply.github.com>
2025-09-22 02:02:32 +0700

    practice data type

---
+++ practice/data_type/practice_one.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+
+    cin >> a;
+    cin >> b;
+
+    // Xuất dữ liệu ra màn hình
+    cout << "Gia tri cua bien thu nhat la " << a << endl;
+    cout << "Gia tri cua bien thu hai la " << b << endl;
+
+    return 0;
+}
